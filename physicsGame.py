6e6e070cafcdd7884af7447f804b05afb88df834
Henry Craig <h.craig52@yahoo.com>
2019-06-25 14:13:44 -0400

    adding some images to get the basic idea down, plus working on rotating the hanging objects

--- physicsGame.py
+++ physicsGame.py
@@ -19,14 +19,48 @@ pygame.init()
 displayWidth = 1000
 displayHeight = 400
 
+white = (255,255,255)
+
 #(width,length)
 gameDisplay = pygame.display.set_mode((displayWidth,displayHeight))
 pygame.display.set_caption("Physics Game")
 gameClock = pygame.time.Clock()
 
-alien = pygame.image.load('BoogerbeingComic.png')
+#Alien info
+alienImg = pygame.image.load('BoogerbeingComic.png')
 def alien(x,y):
-    gameDisplay.blit(alien,(x,y))
+    gameDisplay.blit(alienImg,(x,y))
+alienX = 0
+alienY = 120
+
+#Detective Info
+detectiveImg = pygame.image.load('tracerBullet.png')
+def detective(x,y):
+    gameDisplay.blit(detectiveImg, (x,y))
+detectiveX = 810
+detectiveY = 270
+
+
+pianoImg = pygame.image.load('hangingPiano.png')
+def piano(x,y):
+    gameDisplay.blit(pianoImg, (x,y))
+
+    #This is throwing an error right now, its telling me I'm trying to get it before its referenced?
+
+
+
+pianoX = 200
+pianoY = 0
+
+catImg = pygame.image.load('hangingCat.png')
+def cat(x,y):
+    gameDisplay.blit(catImg, (x,y))
+catX = 800
+catY = 0
+
+
+
+angle = 0
 
 gameEnd = False
 while gameEnd != True:
@@ -35,7 +69,33 @@ while gameEnd != True:
         if event.type == pygame.QUIT:
             gameEnd = True
 
-    alien(0,0)
+    gameDisplay.fill(white)
+
+    detective(detectiveX, detectiveY)
+
+
+    rotated_image = pygame.transform.rotate(pianoImg, angle)
+    angle += 1
+
+    gameDisplay.blit(rotated_image, (pianoX, pianoY))
+
+
+    #Working on getting the center of the top of the image
+    #I actually think it wont be too hard, I just need half the center rotation
+    #and half the top left corner rotation, should be just fine I think
+    w, h = pianoImg.get_size()
+    print(w, h)
+
+    box = [pygame.math.Vector2(p) for p in [(0, 0), (w, 0), (w, -h), (0, -h)]]
+    pianoBox = [pygame.math.Vector2(p) for p in [(pianoX, pianoY), (pianoX + w, pianoY), (pianoX, pianoY), (pianoX, pianoY)]]
+
+    pygame.display.flip()
+
+    cat(catX, catY)
+
+    #Draws the alien to the screen, and slowly moves it across the screen
+    alien(alienX,alienY)
+    alienX += 0.3
 
     pygame.display.update()
     gameClock.tick(60)
--- pygamePrac.py
+++ pygamePrac.py
@@ -1,4 +1,6 @@
 import pygame
+import time
+
 
 pygame.init()
 
@@ -7,64 +9,84 @@ pygame.init()
 displayWidth = 800
 displayHeight = 600
 
-gameDisplay = pygame.display.set_mode((displayWidth, displayHeight))
-#Sets the title of the display, basically the title of the game
-pygame.display.set_caption("A bit Racey")
-
-
 black = (0,0,0)
 white = (255,255,255)
+red = (255,0,0)
 
 car_width = 73
 
+gameDisplay = pygame.display.set_mode((displayWidth, displayHeight))
+pygame.display.set_caption("A bit Racey")                                       #Sets the title of the display, basically the title of the game
+clock = pygame.time.Clock()
+
+
+
 
 #our game clock
-clock = pygame.time.Clock()
-gameExit = False
-carImg = pygame.image.load('BoogerbeingComic.png')
+
+
+carImg = pygame.image.load('racecar.png')
 
 def car(x,y):
     #Apparently blit just draws something to the screen
     gameDisplay.blit(carImg, (x,y))
 
-x = (displayWidth * 0.45)
-y = (displayHeight * 0.8)
-xChange = 0
-carSpeed = 0
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
 
-while not gameExit:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
-
-        #print(event)
-
-    #I think if I was going to have it not exit I would rather do the
-    #handling of edges up here
-    #That way I can just stop the change from happening
-
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_LEFT:
-            xChange = -5
-        elif event.key == pygame.K_RIGHT:
-            xChange = 5
-    if event.type == pygame.KEYUP:
-        if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-            xChange = 0
-
-    x += xChange
-
-
-
-    gameDisplay.fill(white)
-    car(x,y)
-
-    if x > displayWidth - car_width or x < 0:
-        gameExit = True
+def message_display(text):
+    largeText = pygame.font.Font('freesansbold.tff', 115)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((display_width/2), (display_height/2))
+    gameDisplay.blit(TextSurf, TextRect)
 
     pygame.display.update()
-    clock.tick(60)
 
+    time.sleep(2)
+
+    game_loop()                                                     #Our game loop is in here for some reason
+
+def crash():
+    message_display('You Crashed')
+
+def game_loop():
+    x = (displayWidth * 0.45)
+    y = (displayHeight * 0.8)
+
+    xChange = 0
+
+    gameExit = False
+
+
+    while not gameExit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                xChange = -5
+            elif event.key == pygame.K_RIGHT:
+                xChange = 5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                xChange = 0
+
+        x += xChange
+
+        gameDisplay.fill(white)
+        car(x,y)
+
+        if x > displayWidth - car_width or x < 0:
+            gameExit = True
+
+        pygame.display.update()
+        clock.tick(60)
+
+
+game_loop()
 pygame.quit()
 quit()
 
